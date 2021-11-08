7077a2628845f82a69182acc1fd04987162d324f
Felipe Souza <fbsdsgn@gmail.com>
2021-11-08 19:57:01 -0300

    add component Main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,38 @@
+import ProfilePage from '../../components/ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Felipe Souza</strong>
+          <span>613 Tweets</span>
+        </ProfileInfo>
+      </Header>
+      <ProfilePage />
+      <BottomMenu>
+        <HomeIcon className="active" />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
