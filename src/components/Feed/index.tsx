9d2a8c240aa135be0c0a58e3c3191f73cbb14889
Felipe Souza <fbsdsgn@gmail.com>
2021-11-08 19:58:23 -0300

    add component and styles Feed

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,20 @@
+import Tweet from '../Tweet';
+
+import { Container, Tab, Tweets } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+    <Container>
+      <Tab>Tweets</Tab>
+      <Tweets>
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+      </Tweets>
+    </Container>
+  );
+};
+
+export default Feed;
