5234fa5b6606468852cd266084226c1a6d602e57
Felipe Souza <fbsdsgn@gmail.com>
2021-11-08 19:59:25 -0300

    add components and styles SideBar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,46 @@
+import StickyBox from 'react-sticky-box';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="Beck" nickname="@aniebeck" />,
+              <FollowSuggestion name="Natty" nickname="@jurumi" />,
+              <FollowSuggestion name="Yoskhaz" nickname="@yoskhaz" />,
+            ]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+        </Body>
+      </StickyBox>
+    </Container>
+  );
+};
+
+export default SideBar;
